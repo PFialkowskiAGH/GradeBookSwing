23145d0b86a39a5385533486446858d88f9a3f2b
Ayuzawa <pfialkowski@student.agh.edu.pl>
2023-04-13 00:20:37 +0200

    added better class DataGenerator to random generate data

--- src/DataGenerator.java
+++ src/DataGenerator.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 
 public class DataGenerator
 {
+    public static final String[] classes = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
     public static final String[] firstNames = {"Adam", "Paweł", "Patryk", "Kamil", "Joasia", "Anna"};
     public static final String[] lastName = {"Biegała", "Kowalski", "Wiśniewski", "Chory", "Zguba", "Badziewna"};
-    public static final String[] Address = {"Siewna 12", "Pokątna 4", "Krakowskie Przedmieście 12", "Krakowska 8", "Mostowa 12/7", "Aleja Pokoju 12"};
+    public static final String[] addresses = {"Siewna 12", "Pokątna 4", "Krakowskie Przedmieście 12", "Krakowska 8", "Mostowa 12/7", "Aleja Pokoju 12"};
     ClassOfStudentContainer container = new ClassOfStudentContainer();
     public DataGenerator()
     {
@@ -18,7 +19,8 @@ public class DataGenerator
     ClassOfStudent generateClass(Integer i)
     {
         Integer max = getRandomInteger(10, 1);
-        ClassOfStudent classOfStudent = new ClassOfStudent(i.toString(), new ArrayList<>(), max);
+        Integer classLetter = getRandomInteger(10, 1);
+        ClassOfStudent classOfStudent = new ClassOfStudent(i.toString() + classes[classLetter], new ArrayList<>(), max);
         Integer numberOfStudents = max/2;
         for (int j = 0; j <= numberOfStudents; j++)
         {
@@ -29,7 +31,10 @@ public class DataGenerator
     }
     Student generateStudent(Integer j)
     {
-        Student student = new Student(firstNames[j], lastName[j], StudentCondition.Obecny, getRandomInteger(2005, 1997), getRandomInteger(6, 1), Address[j]);
+        Integer firstname = getRandomInteger(5, 1);
+        Integer lastname = getRandomInteger(5, 1);
+        Integer address = getRandomInteger(5, 1);
+        Student student = new Student(firstNames[firstname], lastName[lastname], StudentCondition.Obecny, getRandomInteger(2005, 1997), getRandomInteger(6, 1), addresses[address]);
         return student;
     }
     public static int getRandomInteger(int maximum, int minimum){
